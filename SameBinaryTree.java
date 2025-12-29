53fce78aab5d44dd9b8c3144e49beaef06c0b4fe
shahirsizan <shahir.sizan18@gmail.com>
2025-12-29 12:42:45 +0600

    SameBinaryTree

---
+++ SameBinaryTree.java
@@ -0,0 +1,59 @@
+public class SameBinaryTree {
+	
+	static class TreeNode {
+		int val;
+		TreeNode left, right;
+		
+		public TreeNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	/**
+	 * Time Complexity: O(N); N -> number of nodes. We'll process each node only once.
+	 * Average Space Complexity: O(H); H -> height of the tree (recursion stack).
+	 * Worst Space Complexity: O(N); If skewed tree, then we'll need N recursion stacks
+	 */
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		// if both of them null, they're good to go
+		if (p == null && q == null) {
+			return true;
+		}
+		
+		// if only one of them null OR their values don't match, the tree `not equal`
+		if (p == null || q == null || p.val != q.val) {
+			return false;
+		}
+		
+		// check left and right subtrees recursively
+		boolean leftSame = isSameTree(p.left, q.left);
+		boolean rightSame = isSameTree(p.right, q.right);
+		
+		if (leftSame == true && rightSame == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// ✅ main() ✅
+	public static void main(String[] args) {
+		SameBinaryTree myObj = new SameBinaryTree();
+		
+		TreeNode t1 = new TreeNode(1);
+		t1.left = new TreeNode(2);
+		t1.right = new TreeNode(3);
+		t1.right.right = new TreeNode(4);
+		
+		TreeNode t2 = new TreeNode(1);
+		t2.left = new TreeNode(2);
+		t2.right = new TreeNode(3);
+		t2.right.right = new TreeNode(4);
+		
+		TreeNode t3 = new TreeNode(1);
+		t3.left = new TreeNode(2);
+		
+		System.out.println("Are t1 and t2 the same? " + myObj.isSameTree(t1, t2)); // true
+		System.out.println("Are t1 and t3 the same? " + myObj.isSameTree(t1, t3)); // false
+	}
+}
