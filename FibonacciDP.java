9ed79d1e5ff5269ddd17a585fcaf8cdae6e64a84
shahirsizan <shahir.sizan18@gmail.com>
2026-01-18 19:36:29 +0600

    comments added

--- FibonacciDP.java
+++ FibonacciDP.java
@@ -4,19 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FibonacciDP {
+	// memoization map. We could have used array also because the subproblems have only 1 parameter.
 	Map<Integer, Integer> memoMap = new HashMap<>();
 	
 	public int fib(int n) {
+		// if base case, return `0` or `1`
 		if (n <= 1) {
 			return n;
 		}
 		
+		// not base case, but pre-calculated in the map, return it
 		if (memoMap.containsKey(n)) {
 			return memoMap.get(n);
 		}
 		
+		// not pre-calculated, calculate
 		int value = fib(n - 1) + fib(n - 2);
+		// store the calculation in map
 		memoMap.put(n, value);
+		// return
 		return value;
 	}
 	
