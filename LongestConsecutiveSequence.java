b7d626d6f3a3d5fe4e1592eab49039854c178e06
shahirsizan <shahir.sizan18@gmail.com>
2026-03-02 22:33:36 +0600

    modified

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -1,58 +1,62 @@
-// this solution cracked 79 / 83 testcases
+// CPS academy DSA sheet (Arrays)
+// https://leetcode.com/problems/longest-consecutive-sequence/
 
-import java.util.HashSet;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
-
-	public int longestConsecutive(int[] nums) {
-		if (nums.length == 0) {
+	
+	/**
+	 * 👉 Generate hashset from the given array
+	 * 👉 traverse the hashset
+	 * 👉 for each element `x`, check whether `x-1` exists in the set
+	 * 👉 if NO, we got the starting point of another `continuous sequence`.
+	 * 👉 in a loop, check whether `x+1` exists, if yes increment `local count` and update `global count`.
+	 * 👉 As soon as `x+1` fails, we get the count for an apparent sequence. Get out from the loop.
+	 * 👉 Traverse the set for next staring point.
+	 *
+	 */
+	
+	public int longestConsecutive(int[] givenNums) {
+		
+		if (givenNums.length == 0) {
 			return 0;
 		}
-
-		HashSet<Integer> ourHashSet = new HashSet<>();
-		for (int num : nums) {
+		
+		Set<Integer> ourHashSet = new HashSet<>();
+		for (int num : givenNums) {
 			ourHashSet.add(num);
 		}
-
-		int maxLength = 0;
-		for (int num : nums) {
-//			if `num−1` exists, then `num` is part of an existing sequence, skip.
-//			If `num−1` doesn't exist, then `num` is the starting point of a sequence, track the sequence from this point.
-			if (!ourHashSet.contains(num - 1)) {
-				int currentNum = num;
-				int currentSeqLength = 1;
-				while (ourHashSet.contains(currentNum + 1)) {
-					currentNum += 1;
-					currentSeqLength += 1;
+		
+		// set: {0,  2,3,  6,7,8,9}
+		
+		int globalCount = 1;
+		for (int num : ourHashSet) {
+			int currentStartNum = num;
+			
+			// probable start point of a continuous sequence
+			if (!ourHashSet.contains(currentStartNum - 1)) {
+				int localCount = 1;
+				while (ourHashSet.contains(currentStartNum + 1)) {
+					localCount++;
+					currentStartNum++;
 				}
-				maxLength = Math.max(maxLength, currentSeqLength);
+				globalCount = Math.max(globalCount, localCount);
 			}
+			// no more continuity. Traverse for next starting point of another sequence
 		}
-
-		return maxLength;
+		
+		return globalCount;
 	}
-
-
+	
+	
 	public static void main(String[] args) {
-		LongestConsecutiveSequence solver = new LongestConsecutiveSequence();
-
-		// Example 1:
-		int[] nums1 = {100, 4, 200, 1, 3, 2};
-		int result1 = solver.longestConsecutive(nums1);
-		// Sequence is [1, 2, 3, 4]. Expected: 4
-		System.out.println("--- Example 1 ---");
-		System.out.println("Input: " + Arrays.toString(nums1));
-		System.out.println("Longest Sequence Length: " + result1);
-
-		System.out.println("-----------------");
-
-		// Example 2:
-		int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-		int result2 = solver.longestConsecutive(nums2);
-		// Sequence is [0, 1, 2, 3, 4, 5, 6, 7, 8]. Expected: 9
-		System.out.println("--- Example 2 ---");
-		System.out.println("Input: " + Arrays.toString(nums2));
-		System.out.println("Longest Sequence Length: " + result2);
+		LongestConsecutiveSequence myObj = new LongestConsecutiveSequence();
+		
+		int[] nums = {0, 2, 3, 6, 7, 8, 9};
+		int result = myObj.longestConsecutive(nums);
+		System.out.println("Input: " + Arrays.toString(nums));
+		System.out.println("Longest Sequence: " + result + " (Expected: 4)");
 	}
 }
--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -1,55 +1,57 @@
+// CPS academy DSA sheet (Arrays)
+// https://leetcode.com/problems/top-k-frequent-elements/description/
+
 import java.util.*;
 
 public class TopKFrequentElements {
-    public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> ourHashMap = new HashMap<>();
-//		  populate the hashmap
-        for (int num : nums) {
-            ourHashMap.put(num, ourHashMap.getOrDefault(num, 0) + 1);
-        }
-
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
-                (a, b) -> a.getValue() - b.getValue()
-        );
-
-        for (Map.Entry<Integer, Integer> entry : ourHashMap.entrySet()) {
-			   minHeap.add(entry);
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
-        }
-
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = Objects.requireNonNull(minHeap.poll()).getKey();
-        }
-
-        return result;
-    }
-
-    // --- Main Method for Demonstration ---
-
-    public static void main(String[] args) {
-        TopKFrequentElements solver = new TopKFrequentElements();
-
-        // Example 1:
-        int[] nums1 = {1, 1, 1, 2, 2, 3};
-        int k1 = 2;
-        int[] result1 = solver.topKFrequent(nums1, k1);
-        // Expected: [1, 2] (order may vary)
-        System.out.println("--- Example 1 ---");
-        System.out.println("Input: " + Arrays.toString(nums1) + ", k: " + k1);
-        System.out.println("Result: " + Arrays.toString(result1));
-
-        System.out.println("-----------------");
-
-        // Example 2:
-        int[] nums2 = {4, 1, -1, 2, -1, 2, 3};
-        int k2 = 2;
-        int[] result2 = solver.topKFrequent(nums2, k2);
-        // Frequencies: 1:1, -1:2, 2:2, 3:1. Top 2 are -1 and 2.
-        System.out.println("--- Example 2 ---");
-        System.out.println("Input: " + Arrays.toString(nums2) + ", k: " + k2);
-        System.out.println("Result: " + Arrays.toString(result2));
-    }
+	public int[] topKFrequent(int[] givenNums, int k) {
+		Map<Integer, Integer> ourHashMap = new HashMap<>();
+		
+		// {4, 1, -1, 2, -1, 2, 3}
+		
+		// populate ourHashMap
+		for (int num : givenNums) {
+			if (ourHashMap.containsKey(num)) {
+				ourHashMap.put(num, ourHashMap.get(num) + 1);
+			} else {
+				ourHashMap.put(num, 1);
+			}
+		}
+		
+		// `min heap` will contain least counted element in its head.
+		// So during traversal of the hashmap, all the elements with `less count` will get into the head and polled out.
+		// the last `k` elements that reside in the heap are the `k` largest elements
+		PriorityQueue<Map.Entry<Integer, Integer>> ourMinHeap = new PriorityQueue<>(
+				(a, b) -> a.getValue() - b.getValue()
+		);
+		
+		for (Map.Entry<Integer, Integer> entry : ourHashMap.entrySet()) {
+			ourMinHeap.add(entry);
+			
+			if (ourMinHeap.size() > k) {
+				ourMinHeap.poll();
+			}
+		}
+		
+		int[] result = new int[k];
+		for (int i = 0; i < k; i++) {
+			// Below `Objects.requireNonNull()` were suggested by IDE.
+			// Only `ourMinHeap.poll()).getKey()` would also work.
+			result[i] = Objects.requireNonNull(ourMinHeap.poll()).getKey();
+		}
+		
+		return result;
+	}
+	
+	
+	public static void main(String[] args) {
+		TopKFrequentElements myObj = new TopKFrequentElements();
+		
+		int[] nums = {4, 1, -1, 2, -1, 2, 3};
+		int k = 2;
+		int[] result = myObj.topKFrequent(nums, k);
+		// frequencies: 1:1, -1:2, 2:2, 3:1. Top 2 are -1 and 2.
+		System.out.println("Input: " + Arrays.toString(nums) + ", k: " + k);
+		System.out.println("Result: " + Arrays.toString(result));
+	}
 }
