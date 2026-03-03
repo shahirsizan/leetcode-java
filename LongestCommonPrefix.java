9af39b1d85bfc1fbb59394814592993759afb122
shahirsizan <shahir.sizan18@gmail.com>
2026-03-03 22:35:29 +0600

    modified

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,51 +1,65 @@
+// CPS academy DSA sheet (String)
+// https://leetcode.com/problems/longest-common-prefix/description/
+
 import java.util.Arrays;
 
+/**
+ * Time Complexity: O(S), where S is the sum of all characters in all strings. We visit each character at most once.
+ * Space Complexity: O(L), where L is the length of the first string (to store the StringBuilder).
+ *
+ */
+
 public class LongestCommonPrefix {
-
-	public String longestCommonPrefix(String[] stringList) {
-		if (stringList == null || stringList.length == 0) {
-			return "";
-		}
-
-		String longestPrefix = stringList[0];
-
-		for (int i = 1; i < stringList.length; i++) {
-			String currentWord = stringList[i];
-
-			while (currentWord.indexOf(longestPrefix) != 0) {
-				// as long as index of `longestPrefix` in `currentWord` is not 0,
-				// we remove characters from the back of `longestPrefix` one-by-one
-				if (longestPrefix.length() == 0) {
-					return "";
-				}
-				else{
-					longestPrefix = longestPrefix.substring(0, longestPrefix.length() - 1);
-				}
+	
+	public String longestCommonPrefix(String[] givenStrings) {
+		
+		// start with first string as prefixSB
+		StringBuilder prefixSB = new StringBuilder(givenStrings[0]);
+		
+		// compare current prefixSB to every other string
+		for (int i = 1; i < givenStrings.length; i++) {
+			// constraint edge case. If any of the string length is 0, final answer is ""
+			if (givenStrings[0].length() == 0) {
+				return "";
+			}
+			
+			String currentStr = givenStrings[i];
+			
+			// find the common portion
+			int j = 0;
+			while (j < prefixSB.length()
+					&& j < currentStr.length()
+					&& prefixSB.charAt(j) == currentStr.charAt(j)) {
+				j++;
+			}
+			
+			// trim `prefixSB` to the length of the matching part
+			// setLength() is O(1) (google)
+			prefixSB.setLength(j);
+			
+			// if at any point `prefixSB` becomes empty, return ""
+			if (prefixSB.length() == 0) {
+				return "";
 			}
 		}
-
-		return longestPrefix;
+		
+		return prefixSB.toString();
 	}
-
+	
 	// --- Main Method for Demonstration ---
-
+	
 	public static void main(String[] args) {
-		LongestCommonPrefix solver = new LongestCommonPrefix();
-
-		// Example 1:
+		LongestCommonPrefix myObj = new LongestCommonPrefix();
+		
 		String[] strs1 = {"flower", "flow", "flight"};
-		String result1 = solver.longestCommonPrefix(strs1); // Expected: "fl"
-		System.out.println("--- Example 1 ---");
+		String result1 = myObj.longestCommonPrefix(strs1);
 		System.out.println("Input: " + Arrays.toString(strs1));
-		System.out.println("LCP: \"" + result1 + "\"");
-
-		System.out.println("-----------------");
-
-		// Example 2:
+		System.out.println("Output: \"" + result1 + "\"" + " Expected: \"fl\"");
+		
+		
 		String[] strs2 = {"dog", "racecar", "car"};
-		String result2 = solver.longestCommonPrefix(strs2); // Expected: ""
-		System.out.println("--- Example 2 ---");
+		String result2 = myObj.longestCommonPrefix(strs2);
 		System.out.println("Input: " + Arrays.toString(strs2));
-		System.out.println("LCP: \"" + result2 + "\"");
+		System.out.println("Output: \"" + result2 + "\"" + " Expected: \"\"");
 	}
 }
