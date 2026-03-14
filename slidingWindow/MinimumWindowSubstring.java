3153725d98c7e22a254a952198cfd518956ee18f
shahirsizan <shahir.sizan18@gmail.com>
2026-03-14 16:00:16 +0600

    MinimumWindowSubstring updated

--- slidingWindow/MinimumWindowSubstring.java
+++ slidingWindow/MinimumWindowSubstring.java
@@ -1,23 +1,26 @@
-// CPS academy DSA sheet (String)
+// CPS academy DSA sheet (String, Sliding Window)
 // https://leetcode.com/problems/minimum-window-substring/description/
+
+package slidingWindow;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MinimumWindowSubstring {
 	public String minWindow(String s, String t) {
-		// constraint edge case
+		// edge case
 		if (s.length() < t.length()) {
 			return "";
 		}
 		
-		// map character frequency of `t`
+		// `string t` character frequency
 		Map<Character, Integer> mapT = new HashMap<>();
 		for (char ch : t.toCharArray()) {
 			mapT.put(ch, mapT.getOrDefault(ch, 0) + 1);
 		}
-		
+		// `current window` character frequency
 		Map<Character, Integer> mapCurrentWindow = new HashMap<>();
+		
 		/**
 		 * `haveChars` & `needChars`:
 		 * Keep count (haveChars) of how many unique characters in the mapCurrentWindow matches the required
@@ -30,30 +33,34 @@ public class MinimumWindowSubstring {
 		// res: [shortest substring length, leftIndex, rightIndex]
 		int[] res = {-1, 0, 0};
 		
+		/**
+		 * 1. Locate the very first substring in `large string` where all characters of `short string` are included.
+		 * 2. Then shrink the `left pointer` as long as above property stays valid.
+		 * 3. As soon as above property invalidates, again extend `right pointer`.
+		 * 4. Keep track of the minimum substring `length` in a global count variable
+		 * */
 		int leftPointer = 0;
-		// expand the right pointer
 		for (int rightPointer = 0; rightPointer < s.length(); rightPointer++) {
 			char currentChar = s.charAt(rightPointer);
 			mapCurrentWindow.put(currentChar, mapCurrentWindow.getOrDefault(currentChar, 0) + 1);
 			
-			// If `currentChar` in `currentWindow` matches frequency in string `t`...
+			// If `currentChar` frequency in `currentWindow` matches corresponding frequency in `short string`,
+			// increase `haveChars`
 			if (mapT.containsKey(currentChar) && mapCurrentWindow.get(currentChar).equals(mapT.get(currentChar))) {
 				haveChars++;
 			}
 			
-			// `mapCurrentWindow` is valid
-			// compress the leftPointer pointer as long as it is valid
+			// We enter into below loop as soon as `current substring` contains all characters of the given `short string`
 			while (haveChars == needChars) {
-				// update result if this `mapCurrentWindow` is smaller than previous window
+				// update result if `current window` is smaller than `previous window`
 				int shortestWindowSizeUntilNow = rightPointer - leftPointer + 1;
-				
 				if (res[0] == -1 || shortestWindowSizeUntilNow < res[0]) {
 					res[0] = shortestWindowSizeUntilNow;
 					res[1] = leftPointer;
 					res[2] = rightPointer;
 				}
 				
-				// compress `leftPointer` to find smaller mapCurrentWindow
+				// compress `leftPointer` to find a smaller `current window`
 				char leftChar = s.charAt(leftPointer);
 				mapCurrentWindow.put(leftChar, mapCurrentWindow.get(leftChar) - 1);
 				leftPointer++;
@@ -61,7 +68,6 @@ public class MinimumWindowSubstring {
 				if (mapT.containsKey(leftChar) && mapCurrentWindow.get(leftChar) < mapT.get(leftChar)) {
 					haveChars--;
 				}
-				
 			}
 		}
 		
